c5aa8e2595cf390fbb210ea35c91a6e7735284e0
sveltarne <114943151+sveltarne@users.noreply.github.com>
2022-10-17 11:01:42 -0500

    .

--- src/lib/components/ui/cards/HostingMain.svelte
+++ src/lib/components/ui/cards/HostingMain.svelte
@@ -1,60 +1,36 @@
-
-<div class="card shadow-2xl">
-  <div
-    class="card-body bg-gradient-to-tl from-sky-500 to-fuchsia-600 rounded-xl hero"
-  >
-    <div class="hero-content text-center">
-      <div class="max-w-md">
-        <h1
-          class="font-title text-neutral-focus mb-2 text-4xl font-extrabold sm:text-5xl lg:text-7xl"
-        >
-          Arth Hosting
-        </h1>
-        <p class="py-7 text-neutral-focus">
-          Get your own minecraft server with the most user-friendly setup out
-          there! Plus, both Java & Bedrock Edition players will be able to join!
-        </p>
-        
-        <div class="btn glass text-neutral-focus">
-          <a href="/hosting">Coming Soon</a>
-        </div>
-      </div>
-    </div>
-
- 
-  </div>
-</div>
- <table class="table w-full bg-base-100 table-normal">
+<div class="overflow-x-auto">
+  <table class="table w-full">
     <!-- head -->
     <thead>
       <tr>
+        <th></th>
         <th>Name</th>
-        <th>Price</th>
-        <th>Notes</th>
-        <th>Supports Bedrock/Java Crossplay</th>
+        <th>Job</th>
+        <th>Favorite Color</th>
       </tr>
     </thead>
     <tbody>
       <!-- row 1 -->
       <tr>
-        <th>Arth Hosting</th>
-        <td>$7/Month</td>
-        <td>Plugins, Mods & More for free, easy-to-use panel.</td>
-        <td>Yes, For free.</td>
+        <th>1</th>
+        <td>Cy Ganderton</td>
+        <td>Quality Control Specialist</td>
+        <td>Blue</td>
       </tr>
       <!-- row 2 -->
       <tr>
-        <th>Apex Hosting</th>
-        <td>$10/Month</td>
-        <td>You need to pay $15 to use plugins.</td>
-        <td>Only if you pay for plugins</td>
+        <th>2</th>
+        <td>Hart Hagerty</td>
+        <td>Desktop Support Technician</td>
+        <td>Purple</td>
       </tr>
       <!-- row 3 -->
       <tr>
-        <th>Apex Hosting</th>
-        <td>$10/Month</td>
-        <td>You can't install any mods or plugins, and have a limited amount of players.</td>
-        <td>No</td>
+        <th>3</th>
+        <td>Brice Swyre</td>
+        <td>Tax Accountant</td>
+        <td>Red</td>
       </tr>
     </tbody>
   </table>
+</div>
