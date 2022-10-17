a91a2dba943146bcb684025cb80d03ef0cd3393b
sveltarne <114943151+sveltarne@users.noreply.github.com>
2022-10-17 11:42:52 -0500

    added Bedrock FAQ

--- src/lib/components/ui/cards/HostingFAQ.svelte
+++ src/lib/components/ui/cards/HostingFAQ.svelte
@@ -15,7 +15,8 @@
         </p>
         <ul class="py-7 text-neutral-focus">
         <li>What's a plugin? A Plugin is an add-on to your server that gives it new features, like WorldEdit, which lets you build giant structures & landscapes effortlessly with new brushes & commands.</li>
-        <li>
+        <li>What's a worldgen mod? A worldgen can add new biomes and change the look of a world.</li>
+        <li>If both java & bedrock players can join, how do things like combat work? The experience for java players will be the same, and most things like player heads & modern combat will work as they do on java for Bedock Players.</li>
         </ul>
 
         <div class="btn glass text-neutral-focus">
