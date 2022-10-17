b0057006b0d2adfe13a2b76bdf93b6ce93ce8c09
sveltarne <114943151+sveltarne@users.noreply.github.com>
2022-10-17 10:42:51 -0500

    added hosting page

---
+++ src/routes/hosting/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import Navbar from "$lib/components/layout/Navbar.svelte";
+  import Network from "$lib/components/ui/cards/Network.svelte";
+  import Hosting from "$lib/components/ui/cards/Hosting.svelte";
+  import Mods from "$lib/components/ui/cards/Mods.svelte";
+</script>
+
+<Navbar navType="default" />
+<div class="p-5 space-y-5">
+  <Network />
+  <Hosting />
+  <Mods />
+</div>
