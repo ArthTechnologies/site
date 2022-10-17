85715c56b174551347f66c6f137acf0211437d45
sveltarne <114943151+sveltarne@users.noreply.github.com>
2022-10-17 11:00:32 -0500

    .

--- src/lib/components/ui/cards/HostingMain.svelte
+++ src/lib/components/ui/cards/HostingMain.svelte
@@ -10,14 +10,7 @@
         >
           Arth Hosting
         </h1>
-        <p class="py-7 text-neutral-focus">
-          Get your own minecraft server with the most user-friendly setup out
-          there! Plus, both Java & Bedrock Edition players will be able to join!
-        </p>
         
-        <div class="btn glass text-neutral-focus">
-          <a href="/hosting">Coming Soon</a>
-        </div>
       </div>
     </div>
 
