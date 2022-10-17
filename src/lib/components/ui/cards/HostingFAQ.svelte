cd4916ae8db50874b1158de6d874b9b2434f8737
sveltarne <114943151+sveltarne@users.noreply.github.com>
2022-10-17 11:45:04 -0500

    added bullet points

--- src/lib/components/ui/cards/HostingFAQ.svelte
+++ src/lib/components/ui/cards/HostingFAQ.svelte
@@ -2,7 +2,7 @@
   <div
     class="card-body bg-gradient-to-tl from-sky-500 to-fuchsia-600 rounded-xl hero"
   >
-    <div class="hero-content text-center">
+    <div class="hero-content text-left">
       <div class="max-w-md">
         <h1
           class="font-title text-neutral-focus mb-2 text-4xl font-extrabold sm:text-5xl lg:text-7xl"
@@ -14,9 +14,9 @@
           there! Arth Hosting lets you customize your server with plugins, worldgen mods, and more. Plus, both Java & Bedrock Edition players will be able to join!
         </p>
         <ul class="py-7 text-neutral-focus">
-        <li>What's a plugin? A Plugin is an add-on to your server that gives it new features, like WorldEdit, which lets you build giant structures & landscapes effortlessly with new brushes & commands.</li>
-        <li>What's a worldgen mod? A worldgen can add new biomes and change the look of a world.</li>
-        <li>If both java & bedrock players can join, how do things like combat work? The experience for java players will be the same, and most things like player heads & modern combat will work as they do on java for Bedock Players.</li>
+        <li>• What's a plugin? A Plugin is an add-on to your server that gives it new features, like WorldEdit, which lets you build giant structures & landscapes effortlessly with new brushes & commands.</li>
+        <li>• What's a worldgen mod? A worldgen can add new biomes and change the look of a world.</li>
+        <li>• If both java & bedrock players can join, how do things like combat work? The experience for java players will be the same, and most things like player heads & modern combat will work as they do on java for Bedock Players.</li>
         </ul>
 
         <div class="btn glass text-neutral-focus">
