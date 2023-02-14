beccef99103ecc9b873ea8929d89c65f66c34d03
diamonc <diamonc@proton.me>
2023-02-14 17:53:04 -0600

    added plugin update blog post

--- src/routes/blog/+page.svelte
+++ src/routes/blog/+page.svelte
@@ -20,7 +20,7 @@
 
         document.getElementById("posts").innerHTML = posts
           .map((post) => {
-            return `<a href="/blog/${post.slug}"><div class="card shadow-2xl bg-base-200 hover:bg-base-300 rounded-xl"><p class="text-4xl font-bold card-title text-left ml-5 md:text-center md:justify-center md:ml-0 mt-10">${post.title}</p><div class="card-body  hero"><p class="text-xl">${post.desc}</p></div></div></a>`;
+            return `<a href="/blog/${post.slug}"><div class="card shadow-xl bg-base-200 hover:bg-base-300 rounded-xl mb-10"><p class="text-4xl font-bold card-title text-left ml-5 md:text-center md:justify-center md:ml-0 mt-10">${post.title}</p><div class="card-body  hero"><p class="text-xl">${post.desc}</p></div></div></a>`;
           })
           .join("");
       });
