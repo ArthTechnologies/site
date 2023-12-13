a07ca93bf14396d40e8bb76fb5474209d534c52a
diamonc <diamonc@proton.me>
2023-12-13 12:32:56 -0600

    added experimental rain animation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import { browser } from "$app/environment";
   import { t } from "$lib/scripts/i18n";
 
   import { onMount } from "svelte";
@@ -12,10 +13,60 @@
 
   // Trigger the animation when the component is mounted
   onMount(showElement);
+
+  function createShootingStar() {
+    console.log("createShootingStar");
+    const container = document.getElementById("shootingStarsContainer");
+
+    if (container) {
+      const star = document.createElement("div");
+      star.classList.add("shooting-star");
+      star.classList.add("absolute");
+      star.classList.add("w-1");
+      star.classList.add("h-10");
+      star.classList.add("bg-gradient-to-tr");
+      star.classList.add("from-orange-500");
+      star.classList.add("to-pink-600");
+      star.classList.add("rounded-full");
+      star.classList.add("z-[-1]");
+      star.classList.add("opacity-80");
+
+      const randomX = Math.random() * 100;
+      const randomY = Math.random() * 5 - 5;
+
+      star.style.left = `${randomX}vw`;
+      star.style.top = `${randomY}vh`;
+
+      container.appendChild(star);
+      //play shooting star animation
+      star.animate(
+        [
+          { transform: "translateY(0) rotate(0deg)", opacity: 0 },
+          { opacity: 1 },
+          { transform: "translateY(300vh)", opacity: 0 },
+        ],
+        {
+          duration: 5000,
+          easing: "ease",
+          fill: "forwards",
+        },
+      );
+      setTimeout(() => {
+        star.remove();
+      }, 5000);
+    }
+  }
+
+  if (browser) {
+    //if the browser can handle the animation, this creates a shooting star every 750ms
+    if (window.matchMedia("(prefers-reduced-motion: no-preference)").matches) {
+      setInterval(createShootingStar, 600);
+    }
+  }
 </script>
 
 <div
-  class="bg-black flex flex-col items-center min-h-screen text-[#efefef] pb-48 px-5"
+  class="bg-black flex flex-col items-center min-h-screen text-[#efefef] pb-48 px-5 mainArea"
 >
   <div class="text-5xl font-bold mt-24 text-center space-y-5">
     <span
@@ -31,7 +82,7 @@
       <img
         src="/images/ServerCard.webp"
         alt="Server Card from our Interface"
-        class=""
+        class="z-20"
         width="500px"
       />
 
@@ -56,14 +107,14 @@
       <img
         src="/images/PhoneMinecraft.webp"
         alt="Minecraft on a phone, looking at a java edition player."
-        class="w-[10rem] md:w-[30rem] -mb-8"
+        class="w-[10rem] md:w-[30rem] -mb-8 z-20"
       />
     </div>
     <div class="flex items-center mt-24 space-x-3 md:ml-48 element-container">
       <img
         src="/images/RyzenCPU.webp"
         alt="AMD Ryzen CPU"
-        class="w-[10rem] md:w-[30rem]"
+        class="w-[10rem] md:w-[30rem] z-20"
       />
       <div class="w-64">
         <p class="text-xl font-bold">{$t("main.performance.title")}</p>
@@ -127,6 +178,10 @@
       <sup>4</sup>{$t("main.footnote4")}
     </li>
   </ul>
+  <div
+    class="shooting-stars-container relative"
+    id="shootingStarsContainer"
+  ></div>
 </div>
 <!--vertical gradient from black to base 300-->
 <div class="h-48 bg-gradient-to-b from-black to-base-300" />
@@ -146,6 +201,30 @@
     100% {
       opacity: 1;
       transform: translateY(0);
+    }
+  }
+
+  .shooting-stars-container {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    pointer-events: none;
+    z-index: 10; /* Adjust the z-index as needed */
+  }
+
+  @keyframes shootingStarAnimation {
+    0% {
+      transform: translateY(0) rotate(0deg);
+      opacity: 0;
+    }
+    50% {
+      opacity: 1;
+    }
+    100% {
+      transform: translateY(100vh) rotate(720deg);
+      opacity: 0;
     }
   }
 </style>
