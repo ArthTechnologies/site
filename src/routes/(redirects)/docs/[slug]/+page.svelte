f6856336c4d461de864649ce61765769c837cf1a
DiamonC <diamonc@proton.me>
2024-08-17 21:21:41 -0500

    fixed old /docs links not working

---
+++ src/routes/(redirects)/docs/[slug]/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+  import { browser } from "$app/environment";
+  import { goto } from "$app/navigation";
+
+  if (browser) {
+    let slug = window.location.pathname.split("/")[2];
+    goto("/software/" + slug);
+  }
+</script>
