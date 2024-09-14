cd86be35941c477abf05c3528567c1ca98b93a1b
DiamonC <diamonc@proton.me>
2024-09-14 13:30:24 -0500

    added sponsor kit page

---
+++ src/routes/sponsor-kit/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import { browser } from "$app/environment";
+  import { marked } from "marked";
+  let t;
+  let metadata;
+  let version;
+  let published;
+  let effective;
+
+  if (browser) {
+    fetch("https://backend.arthmc.xyz/file/docs/sponsor-kit.md")
+      .then((response) => response.text())
+      .then((text) => {
+        t = marked(text);
+        version = metadata.version;
+        published = metadata.published;
+        effective = metadata.effective;
+      });
+  }
+</script>
+
+<article class="prose p-5">
+  {@html t}
+</article>
