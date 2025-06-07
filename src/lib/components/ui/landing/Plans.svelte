e50361e4c07c70c0cb9cf0971e312d1499f218cd
DiamonC <diamonc@proton.me>
2025-06-07 18:46:37 -0500

    numerous visual improvements for plan cards

--- src/lib/components/ui/landing/Plans.svelte
+++ src/lib/components/ui/landing/Plans.svelte
@@ -32,13 +32,13 @@
       {$t("landing.plans.basic.title")}
     </p>
 
-    <div class=" flex gap-5 md:mb-6">
-      <div class="max-md:hidden w-24 outline p-2 rounded">
-        <p class="text-center font-poppins-bold text-xl">4GB</p>
+    <div class=" flex gap-4 -mb-3.5 md:mb-3.5">
+      <div class="max-md:hidden w-20 outline p-2 rounded">
+        <p class="text-center font-poppins-bold text-xl ">4GB</p>
         <div
-          class="flex font-poppins font-bold items-center gap-1 justify-center"
+          class="flex font-poppins font-bold items-center gap-1 justify-center text-sm"
         >
-          <MemoryStick size="20" />RAM
+          <MemoryStick size="18" />RAM
         </div>
       </div>
       <div class="md:hidden p-2 rounded">
@@ -49,17 +49,17 @@
           <MemoryStick size="20" />4GB
         </div>
       </div>
-      <div class="w-24 outline p-2 rounded max-md:hidden">
-        <p class="text-center font-poppins-bold text-xl">Ryzen 7</p>
+      <div class="w-24 outline outline-[#d1d1d1] p-2 rounded max-md:hidden">
+        <p class="text-center font-poppins-bold text-xl">10GB</p>
         <div
-          class="flex font-poppins font-bold items-center gap-1 justify-center"
+          class="flex font-poppins font-bold items-center gap-0.5 justify-center text-sm"
         >
-          <CpuIcon size="20" />CPU
+          <CpuIcon size="16" />Storage
         </div>
       </div>
     </div>
-    <p class="font-poppins-bold text-3xl md:text-5xl text-center max-md:flex flex-col">
-      $3.99<span class="text-sm">/{$t("misc.month")}</span>
+    <p class="font-poppins-bold text-3xl md:text-[51px] mt-2 text-center max-md:flex flex-col mb-1.5">
+      $3.99<span class="text-sm">/{$t("misc.monthShort")}</span>
     </p>
     <a
       on:click={getStartedClicked}
@@ -68,7 +68,7 @@
       rel="noreferrer"
     >
       <div
-        class="text-sm w-fit h-12 px-5 cursor-pointer flex items-center bg-gradient-to-b from-[#E93843] to-[#F56922] hover:brightness-90 rounded-full text-gray-300 whiteGradientStroke font-poppins-bold"
+        class="text-sm w-fit h-12 px-5 cursor-pointer flex items-center bg-gradient-to-b from-[#E93843] to-[#F56922] hover:brightness-90 rounded-full text-gray-300 whiteGradientStroke font-poppins-bold text-[#ebeef5]"
       >
         {$t("landing.plans.button")}
       </div>
@@ -80,14 +80,13 @@
     <p class="font-poppins-bold text-[1.25rem] md:text-3xl text-center -mb-3 md:mb-3">
       {$t("landing.plans.plus.title")}
     </p>
-
-    <div class=" flex gap-5 md:mb-6">
-      <div class="max-md:hidden w-24 outline p-2 rounded">
-        <p class="text-center font-poppins-bold text-xl">6GB</p>
+    <div class=" flex gap-4 -mb-3.5 md:mb-3.5">
+      <div class="max-md:hidden w-20 outline p-2 rounded">
+        <p class="text-center font-poppins-bold text-xl ">8GB</p>
         <div
-          class="flex font-poppins font-bold items-center gap-1 justify-center"
+          class="flex font-poppins font-bold items-center gap-1 justify-center text-sm"
         >
-          <MemoryStick size="20" />RAM
+          <MemoryStick size="18" />RAM
         </div>
       </div>
       <div class="md:hidden p-2 rounded">
@@ -98,17 +97,17 @@
           <MemoryStick size="20" />6GB
         </div>
       </div>
-      <div class="w-24 outline p-2 rounded max-md:hidden">
-        <p class="text-center font-poppins-bold text-xl">Ryzen 7</p>
+      <div class="w-24 outline outline-[#d1d1d1] p-2 rounded max-md:hidden">
+        <p class="text-center font-poppins-bold text-xl">15GB</p>
         <div
-          class="flex font-poppins font-bold items-center gap-1 justify-center"
+          class="flex font-poppins font-bold items-center gap-0.5 justify-center text-sm"
         >
-          <CpuIcon size="20" />CPU
+          <CpuIcon size="16" />Storage
         </div>
       </div>
     </div>
-    <p class="font-poppins-bold text-3xl md:text-5xl text-center max-md:flex flex-col">
-      $5.99<span class="text-sm">/{$t("misc.month")}</span>
+    <p class="font-poppins-bold text-3xl md:text-[51px] mt-2 text-center max-md:flex flex-col mb-1.5">
+      $5.99<span class="text-sm">/{$t("misc.monthShort")}</span>
     </p>
     <a
       on:click={getStartedClicked}
@@ -117,26 +116,26 @@
       rel="noreferrer"
     >
       <div
-        class="text-sm w-fit h-12 px-5 cursor-pointer flex items-center bg-gradient-to-b from-[#E93843] to-[#F56922] hover:brightness-90 rounded-full text-gray-300 whiteGradientStroke font-poppins-bold"
+        class="text-sm w-fit h-12 px-5 cursor-pointer flex items-center bg-gradient-to-b from-[#E93843] to-[#F56922] hover:brightness-90 rounded-full text-gray-300 whiteGradientStroke font-poppins-bold text-[#ebeef5]"
       >
         {$t("landing.plans.button")}
       </div>
     </a>
   </div>
   <div
-    class="max-md:w-48 flex flex-col items-center gap-2.5 bg-base-300 bg-opacity-[85%] backdrop-blur-[1px] rounded-2xl px-3 md:px-6 py-5 grayGradientStroke"
+    class="max-md:w-48 flex flex-col items-center gap-2.5 bg-base-300 bg-opacity-[85%] backdrop-blur-[1px] rounded-2xl px-3 md:px-6 py-5 goldGradientStroke"
   >
     <p class="font-poppins-bold text-[1.25rem] md:text-3xl text-center -mb-3 md:mb-3">
       {$t("landing.plans.premium.title")}
     </p>
 
-    <div class=" flex gap-5 md:mb-6">
-      <div class="max-md:hidden w-24 outline p-2 rounded">
-        <p class="text-center font-poppins-bold text-xl">8GB</p>
+    <div class=" flex gap-4 -mb-3.5 md:mb-3.5">
+      <div class="max-md:hidden w-20 outline p-2 rounded outline-[#edcfb0] text-[#edcfb0]">
+        <p class="text-center font-poppins-bold text-xl ">8GB</p>
         <div
-          class="flex font-poppins font-bold items-center gap-1 justify-center"
+          class="flex font-poppins font-bold items-center gap-1 justify-center text-sm"
         >
-          <MemoryStick size="20" />RAM
+          <MemoryStick size="18" />RAM
         </div>
       </div>
       <div class="md:hidden p-2 rounded">
@@ -147,17 +146,17 @@
           <MemoryStick size="20" />8GB
         </div>
       </div>
-      <div class="w-24 outline p-2 rounded max-md:hidden">
-        <p class="text-center font-poppins-bold text-xl">Ryzen 7</p>
+      <div class="w-24 outline outline-[#d1d1d1] p-2 rounded max-md:hidden">
+        <p class="text-center font-poppins-bold text-xl">20GB</p>
         <div
-          class="flex font-poppins font-bold items-center gap-1 justify-center"
+          class="flex font-poppins font-bold items-center gap-0.5 justify-center text-sm"
         >
-          <CpuIcon size="20" />CPU
+          <CpuIcon size="16" />Storage
         </div>
       </div>
     </div>
-    <p class="font-poppins-bold text-3xl md:text-5xl text-center max-md:flex flex-col">
-      $7.99<span class="text-sm">/{$t("misc.month")}</span>
+    <p class="font-poppins-bold text-3xl md:text-[51px] mt-2 text-center max-md:flex flex-col mb-1.5">
+      $7.99<span class="text-sm">/{$t("misc.monthShort")}</span>
     </p>
     <a
       on:click={getStartedClicked}
@@ -166,7 +165,7 @@
       rel="noreferrer"
     >
       <div
-        class="text-sm w-fit h-12 px-5 cursor-pointer flex items-center bg-gradient-to-b from-[#E93843] to-[#F56922] hover:brightness-90 rounded-full text-gray-300 whiteGradientStroke font-poppins-bold"
+        class="text-sm w-fit h-12 px-5 cursor-pointer flex items-center bg-gradient-to-b from-[#E93843] to-[#F56922] hover:brightness-90 rounded-full text-gray-300 whiteGradientStroke font-poppins-bold text-[#ebeef5]"
       >
         {$t("landing.plans.button")}
       </div>
@@ -217,6 +216,31 @@
     border-radius: inherit; /* Inherits button's border-radius */
     padding: 2px; /* Space between button and border */
     background: linear-gradient(180deg, #ffffff, #ffffff00);
+    -webkit-mask:
+      linear-gradient(#fff 0 0) content-box,
+      linear-gradient(#fff 0 0);
+    -webkit-mask-composite: destination-out;
+    mask-composite: exclude;
+    z-index: -1;
+  }
+
+    .goldGradientStroke {
+    position: relative;
+
+    z-index: 1;
+  }
+
+  .goldGradientStroke::before {
+    content: "";
+    position: absolute;
+    top: 0px;
+
+    bottom: 0px;
+    left: 0px;
+    right: 0px;
+    border-radius: inherit; /* Inherits button's border-radius */
+    padding: 2px; /* Space between button and border */
+    background: linear-gradient(180deg, #f1d6b8, #d09670);
     -webkit-mask:
       linear-gradient(#fff 0 0) content-box,
       linear-gradient(#fff 0 0);
